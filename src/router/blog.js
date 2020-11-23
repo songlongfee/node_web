5779993174b943b338d44477c6454b8419276e1f
songlongfei <songlfneu@163.com>
2020-11-23 22:40:34 +0800

    add: add router

---
+++ src/router/blog.js
@@ -0,0 +1,40 @@
+const handleBlogRouter = (req, res) => {
+  const method = req.method // GET POST
+
+  // 获取博客列表
+  if(method === 'GET' && req.path === '/api/blog/list') {
+    return {
+      msg: '博客列表接口'
+    }
+  }
+
+  // 获取博客详情
+  if(method === 'GET' && req.path === '/api/blog/detail') {
+    return {
+      msg: '博客详情接口'
+    }
+  }
+
+  // 新建博客
+  if(method === 'POST' && req.path === '/api/blog/add') {
+    return {
+      msg: '新建博客接口'
+    }
+  }
+
+  // 编辑博客
+  if(method === 'POST' && req.path === '/api/blog/update') {
+    return {
+      msg: '编辑博客接口'
+    }
+  }
+
+  // 删除博客
+  if(method === 'POST' && req.path === '/api/blog/del') {
+    return {
+      msg: '删除博客接口'
+    }
+  }
+}
+
+module.exports = handleBlogRouter
---
+++ src/router/user.js
@@ -0,0 +1,12 @@
+const handleUserRouter = (req, res) => {
+  const method = req.method // GET POST
+
+  // 登录
+  if(method === 'POST' && req.path === '/api/user/login') {
+    return {
+      msg: '登录接口'
+    }
+  }
+}
+
+module.exports  = handleUserRouter
